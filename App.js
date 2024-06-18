bc0e7d97bf65a6ae5433932ccb547306cebb1a0f
Agustin Rodriguez <agustinrodriguez@192.168.1.208>
2024-06-18 11:03:52 -0300

    primer commit rumbo al proyecto final

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Proyecto Final colección HotWheels</Text>
       <StatusBar style="auto" />
     </View>
   );
